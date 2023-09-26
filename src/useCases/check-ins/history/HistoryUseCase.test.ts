30cb6749b6673e9f0fb9fd75b907a1aeeccd79e0
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-09-25 21:23:47 -0300

    fix: little adjusts

--- src/useCases/check-ins/history/HistoryUseCase.test.ts
+++ src/useCases/check-ins/history/HistoryUseCase.test.ts
@@ -35,7 +35,7 @@ describe("User check in history useCase", () => {
   });
 
   it("should be able to fetch paginated check-in history", async () => {
-    for (let i = 0; i <= 22; i++) {
+    for (let i = 1; i <= 22; i++) {
       await checkInsRepository.create({
         gym_id: `gym-${i}`,
         user_id: "user-01",
@@ -49,8 +49,8 @@ describe("User check in history useCase", () => {
 
     expect(checkIns).toHaveLength(2);
     expect(checkIns).toEqual([
-      expect.objectContaining({ gym_id: "gym-01" }),
-      expect.objectContaining({ gym_id: "gym-02" }),
+      expect.objectContaining({ gym_id: "gym-21" }),
+      expect.objectContaining({ gym_id: "gym-22" }),
     ]);
   });
 });

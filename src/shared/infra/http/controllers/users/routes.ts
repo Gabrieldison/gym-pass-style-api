57eaaddd4821f2fabbb29eb4e96fd94b3ad6fca1
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-09-27 14:50:25 -0300

    feat: implement refresh token in API

--- src/shared/infra/http/controllers/users/routes.ts
+++ src/shared/infra/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { registerController } from "./register/RegisterController";
 import { authenticateController } from "./authenticate/AuthenticateController";
 import { ProfileController } from "./get-user-profile/ProfileController";
 import { verifyJwt } from "../../middlewares/verify-jwt";
+import { refreshTokenController } from "./authenticate/RefreshTokenController";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateController);
+  app.patch('/token/refresh', refreshTokenController)
   app.get("/me", { onRequest: [verifyJwt] }, ProfileController);
 }
